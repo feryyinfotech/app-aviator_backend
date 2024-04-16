c196c60921acc3180c247cd663787ece175c5024
feryyinfotech <101308921+feryyinfotech@users.noreply.github.com>
2024-04-16 18:38:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const todoRoutes = require("./routes/todos");
+const moment = require("moment");
+const soment = require("moment-timezone");
 require("dotenv").config();
-const conn = require("./config/database");
-const schedule = require('node-schedule');
-const axios = require('axios');
+const schedule = require("node-schedule");
+const { default: axios } = require("axios");
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
@@ -30,26 +31,14 @@ const PORT = process.env.PORT || 4000;
 
 app.use("/api/v1", todoRoutes);
 
-try {
-  conn.connect((err) => {
-    if (err) {
-      console.error("Error connecting to the database:", err);
-    } else {
-      console.log("Connected to the database");
-    }
-  });
-} catch (e) {
-  console.error("Error:", e);
-}
-
 const array = [
   2, 20, 2, 30, 2, 60, 10, 2, 3, 18, 2, 17, 12, 40, 10, 2, 5, 3, 2, 2, 12, 13,
-  10, 2, 2,2, 20, 50, 2,2,
+  10, 2, 2, 2, 20, 50, 2, 2,
 ];
 
 function generateAndSendMessage() {
   const value = Math.floor(Math.random() * array.length - 1) + 1;
-  const time =  array[value] || 12;
+  const time = array[value] || 12;
   io.emit("message", time);
 
   let fly_time = 0;
@@ -99,117 +88,284 @@ function generateAndSendMessage() {
 
 // color prediction game time generated every 1 min
 function generatedTimeEveryAfterEveryOneMin() {
-
-  let seconds = 59;
-  const interval = setInterval(() => {
-    io.emit("onemin", seconds);
-    console.log(seconds,"THis is message");
-    // console.log("time",seconds);
-    seconds--;
-    if (seconds < 0) {
-      seconds = 59;
-      clearInterval(interval);
-      generatedTimeEveryAfterEveryOneMin();
-    }
-  }, 1000);
+  const job = schedule.scheduleJob("* * * * * *", function () {
+    const currentTime = new Date();
+    const timeToSend =
+      currentTime.getSeconds() > 0
+        ? 60 - currentTime.getSeconds()
+        : currentTime.getSeconds();
+    io.emit("onemin", timeToSend); // Emit the formatted time
+  });
 }
 
 // color prediction game time generated every 3 min
 const generatedTimeEveryAfterEveryThreeMin = () => {
   let min = 2;
-  let sec = 59;
 
-  const interval = setInterval(() => {
-    io.emit("threemin", `${min}_${sec}`);
-    sec--;
-
-    if (sec < 0) {
-      sec = 59;
+  const job = schedule.scheduleJob("* * * * * *", function () {
+    const currentTime = new Date().getSeconds(); // Get the current time
+    const timeToSend = currentTime > 0 ? 60 - currentTime : currentTime;
+    io.emit("threemin", `${min}_${timeToSend}`);
+    if (currentTime === 0) {
       min--;
-
-      if (min < 0) {
-        sec = 59;
-        min = 2;
-        clearInterval(interval);
-        generatedTimeEveryAfterEveryThreeMin();
-      }
+      if (min < 0) min = 2; // Reset min to 2 when it reaches 0
     }
-  }, 1000);
+  });
 };
 
 const generatedTimeEveryAfterEveryFiveMin = () => {
   let min = 4;
-  let sec = 59;
 
-  const interval = setInterval(() => {
-    io.emit("fivemin", `${min}_${sec}`);
-
-    sec--;
-
-    if (sec < 0) {
-      sec = 59;
+  const job = schedule.scheduleJob("* * * * * *", function () {
+    const currentTime = new Date().getSeconds(); // Get the current time
+    const timeToSend = currentTime > 0 ? 60 - currentTime : currentTime;
+    io.emit("fivemin", `${min}_${timeToSend}`);
+    if (currentTime === 0) {
       min--;
-
-      if (min < 0) {
-        sec = 59;
-        min = 4;
-        clearInterval(interval);
-        generatedTimeEveryAfterEveryFiveMin();
-      }
+      if (min < 0) min = 4; // Reset min to 2 when it reaches 0
     }
-  }, 1000);
+  });
 };
 
+// TRX
+// color prediction game time generated every 1 min
+function generatedTimeEveryAfterEveryOneMinTRX() {
+  let three = 0;
+  let five = 0;
+  const rule = new schedule.RecurrenceRule();
+  rule.second = new schedule.Range(0, 59);
+  const job = schedule.scheduleJob(rule, function () {
+    const currentTime = new Date();
+    const timeToSend =
+      currentTime.getSeconds() > 0
+        ? 60 - currentTime.getSeconds()
+        : currentTime.getSeconds();
+    io.emit("onemintrx", timeToSend);
+    if (timeToSend === 6) {
+      const datetoAPISend = parseInt(new Date().getTime().toString());
+      const actualtome = soment.tz("Asia/Kolkata");
+      const time = actualtome.add(8, "hours").valueOf();
 
-// Schedule the function to run daily at 12:00 AM 0 0 * * *
-const job = schedule.scheduleJob('0 1 * * *', async function() {
-  try {
-    // Make the API call using axios
-    await axios.get('https://admin.gameszone.life/api/wallet-income')
-     setTimeout(async ()=>{
-      try{
-        await axios.get("https://admin.gameszone.life/api/bet-income");
-      }catch(e){
-        console.log(e)
+      try {
+        if (three === 2) {
+          three = 0;
+        } else {
+          three++;
+        }
+
+        if (five === 4) {
+          five = 0;
+        } else {
+          five++;
+        }
+        setTimeout(async () => {
+          const res = await axios.get(
+            `https://apilist.tronscanapi.com/api/block?sort=-balance&start=0&limit=20&producer=&number=&start_timestamp=${datetoAPISend}&end_timestamp=${datetoAPISend}`
+          );
+          if (res?.data?.data[0]) {
+            const obj = res.data.data[0];
+            const fd = new FormData();
+            fd.append("hash", `**${obj.hash.slice(-4)}`);
+            fd.append("digits", `${obj.hash.slice(-5)}`);
+            fd.append("number", obj.number);
+            fd.append("time", moment(time).format("HH:mm:ss"));
+
+            const newString = obj.hash;
+            let num = null;
+            for (let i = newString.length - 1; i >= 0; i--) {
+              if (!isNaN(parseInt(newString[i]))) {
+                num = parseInt(newString[i]);
+                break;
+              }
+            }
+            fd.append("slotid", num);
+            fd.append("overall", JSON.stringify(obj));
+            //  trx 1
+            try {
+              const response = await axios.post(
+                "https://zupeeter.com/Apitrx/insert_one_trx",
+                fd
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }
+        }, [6000]);
+      } catch (e) {
+        console.log(e);
       }
-    },5000)
-    setTimeout(async ()=>{
-      try{
-       await axios.get("https://admin.gameszone.life/api/direct-income");
-      }catch(e){
-        console.log(e)
+    }
+  });
+}
+
+// generatedTimeEveryAfterEveryOneMinTRX();
+
+const generatedTimeEveryAfterEveryThreeMinTRX = () => {
+  let min = 2;
+  const job = schedule.scheduleJob("* * * * * *", function () {
+    const currentTime = new Date().getSeconds(); // Get the current time
+    const timeToSend = currentTime > 0 ? 60 - currentTime : currentTime;
+    io.emit("threemintrx", `${min}_${timeToSend}`);
+    if (min === 0 && timeToSend === 6) {
+      const datetoAPISend = parseInt(new Date().getTime().toString());
+      const actualtome = soment.tz("Asia/Kolkata");
+      const time = actualtome.add(8, "hours").valueOf();
+      try {
+        setTimeout(async () => {
+          const res = await axios.get(
+            `https://apilist.tronscanapi.com/api/block?sort=-balance&start=0&limit=20&producer=&number=&start_timestamp=${datetoAPISend}&end_timestamp=${datetoAPISend}`
+          );
+          if (res?.data?.data[0]) {
+            const obj = res.data.data[0];
+            const fd = new FormData();
+            fd.append("hash", `**${obj.hash.slice(-4)}`);
+            fd.append("digits", `${obj.hash.slice(-5)}`);
+            fd.append("number", obj.number);
+            fd.append("time", moment(time).format("HH:mm:ss"));
+            const newString = obj.hash;
+            let num = null;
+            for (let i = newString.length - 1; i >= 0; i--) {
+              if (!isNaN(parseInt(newString[i]))) {
+                num = parseInt(newString[i]);
+                break;
+              }
+            }
+            fd.append("slotid", num);
+            fd.append("overall", JSON.stringify(obj));
+            //  trx 3
+            try {
+              console.log("functoin call for 3 min")
+              const response = await axios.post(
+                "https://zupeeter.com/Apitrx/insert_three_trx",
+                fd
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }
+        }, [6000]);
+      } catch (e) {
+        console.log(e);
       }
-    },10000)
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
-});
+    }
+    if (currentTime === 0) {
+      min--;
+      if (min < 0) min = 2; // Reset min to 2 when it reaches 0
+    }
+  });
+};
 
+const generatedTimeEveryAfterEveryFiveMinTRX = () => {
+  let min = 4;
+  const job = schedule.scheduleJob("* * * * * *", function () {
+    const currentTime = new Date().getSeconds(); // Get the current time
+    const timeToSend = currentTime > 0 ? 60 - currentTime : currentTime;
+    io.emit("fivemintrx", `${min}_${timeToSend}`);
+    if (min === 0 && timeToSend === 6) {
+      const datetoAPISend = parseInt(new Date().getTime().toString());
+      const actualtome = soment.tz("Asia/Kolkata");
+      const time = actualtome.add(8, "hours").valueOf();
+      try {
+        setTimeout(async () => {
+          const res = await axios.get(
+            `https://apilist.tronscanapi.com/api/block?sort=-balance&start=0&limit=20&producer=&number=&start_timestamp=${datetoAPISend}&end_timestamp=${datetoAPISend}`
+          );
+          if (res?.data?.data[0]) {
+            const obj = res.data.data[0];
+            const fd = new FormData();
+            fd.append("hash", `**${obj.hash.slice(-4)}`);
+            fd.append("digits", `${obj.hash.slice(-5)}`);
+            fd.append("number", obj.number);
+            fd.append("time", moment(time).format("HH:mm:ss"));
+            const newString = obj.hash;
+            let num = null;
+            for (let i = newString.length - 1; i >= 0; i--) {
+              if (!isNaN(parseInt(newString[i]))) {
+                num = parseInt(newString[i]);
+                break;
+              }
+            }
+            fd.append("slotid", num);
+            fd.append("overall", JSON.stringify(obj));
+            //  trx 3
+            try {
+              console.log("functoin call for 5 min")
+              const response = await axios.post(
+                "https://zupeeter.com/Apitrx/insert_five_trx",
+                fd
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }
+        }, [6000]);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    if (currentTime === 0) {
+      min--;
+      if (min < 0) min = 4; // Reset min to 4 when it reaches 0
+    }
+  });
+};
 
-
+io.on("connection", (socket) => {});
 
 let x = true;
-io.on("connection", (socket) => {
-  // if (x) {
-  //   console.log("Functions called");
-  //   generateAndSendMessage(); // aviator game every random time
-  //   generatedTimeEveryAfterEveryOneMin(); // color prediction game every 1 time generating time
-  //   generatedTimeEveryAfterEveryThreeMin(); // color prediction game every 3 time generating time
-  //   generatedTimeEveryAfterEveryFiveMin(); // color prediction game every 5 time generating time
-  //   x = false;
-  // }
-});
+let trx = true;
 
-if(x){
-  generateAndSendMessage(); // aviator game every random time
-  generatedTimeEveryAfterEveryOneMin(); // color prediction game every 1 time generating time
-  generatedTimeEveryAfterEveryThreeMin(); // color prediction game every 3 time generating time
-  generatedTimeEveryAfterEveryFiveMin(); 
-  x = false;
+// console.log(time,moment(time).format("HH:mm:ss"))
+if (trx) {
+  const now = new Date();
+  const nowIST = soment(now).tz("Asia/Kolkata");
+  // const fiveHoursThirtyMinutesLater = nowIST.clone().add(5, 'hours').add(30, 'minutes');
+
+  const currentMinute = nowIST.minutes();
+  const currentSecond = nowIST.seconds();
+
+  // Calculate remaining minutes and seconds until 22:28 IST
+  const minutesRemaining = 45 - currentMinute - 1;
+  const secondsRemaining = 60 - currentSecond;
+
+  const delay = (minutesRemaining * 60 + secondsRemaining) * 1000;
+  console.log(minutesRemaining, secondsRemaining, delay);
+
+  setTimeout(() => {
+    generatedTimeEveryAfterEveryOneMinTRX();
+    generatedTimeEveryAfterEveryThreeMinTRX();
+    generatedTimeEveryAfterEveryFiveMinTRX();
+    trx = false;
+  }, delay);
+}
+// generatedTimeEveryAfterEveryThreeMinTRX();
+
+// const job = schedule.scheduleJob(rule, function () {
+//   if (x) {
+//     console.log("Function is called now")
+//     generateAndSendMessage();
+//     generatedTimeEveryAfterEveryOneMin();
+//     generatedTimeEveryAfterEveryThreeMin();
+//     generatedTimeEveryAfterEveryFiveMin();
+//     x = false;
+//   }
+// });
+
+if (x) {
+  // generateAndSendMessage();
+  console.log("Waiting for the next minute to start...");
+  const now = new Date();
+  const secondsUntilNextMinute = 60 - now.getSeconds();
+  setTimeout(() => {
+    generatedTimeEveryAfterEveryOneMin();
+    generatedTimeEveryAfterEveryThreeMin();
+    generatedTimeEveryAfterEveryFiveMin();
+    x = false;
+  }, secondsUntilNextMinute * 1000);
 }
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Socket Run at ${PORT}</h1>`);
+  res.send(`<h1>server running at port=====> ${PORT}</h1>`);
 });
 
 httpServer.listen(PORT, () => {
